0a24ca0ec3a2f644d0865617c42181350e3b673c
ikshugyawali <ikshugyawali@gmail.com>
2022-03-12 02:15:50 +0545

    Relative Layout

---
+++ app/src/main/java/tbc/dma/hellotoast/RelativeLayout.java
@@ -0,0 +1,21 @@
+package tbc.dma.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RelativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+
+    public void showToast(View view) {
+    }
+
+    public void countUp(View view) {
+    }
+}
